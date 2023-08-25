b0d21b5d28b77d39706c7607922067fd971399c3
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-24 17:23:59 -0700

    small chnages

--- La_functions.c
+++ La_functions.c
@@ -8,33 +8,33 @@
  */
 stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
-	stack_t *temp, *aux;
+    stack_t *temp, *aux;
 
-	if (head == NULL)
-		return (NULL);
-	temp = malloc(sizeof(stack_t));
-	if (!temp)
-	{
-		dprintf(2, "Error: malloc failed\n");
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
-	temp->n = n;
-	/*Careful with the first time*/
-	if (*head == NULL)
-	{
-		temp->next = *head;
-		temp->prev = NULL;
-		*head = temp;
-		return (*head);
-	}
-	aux = *head;
-	while (aux->next)
-		aux = aux->next;
-	temp->next = aux->next;
-	temp->prev = aux;
-	aux->next = temp;
-	return (aux->next);
+    if (head == NULL)
+        return (NULL);
+    temp = malloc(sizeof(stack_t));
+    if (!temp)
+    {
+        dprintf(2, "Error: malloc failed\n");
+        free_montyGlobals();
+        exit(EXIT_FAILURE);
+    }
+    temp->n = n;
+    /*Careful with the first time*/
+    if (*head == NULL)
+    {
+        temp->next = *head;
+        temp->prev = NULL;
+        *head = temp;
+        return (*head);
+    }
+    aux = *head;
+    while (aux->next)
+        aux = aux->next;
+    temp->next = aux->next;
+    temp->prev = aux;
+    aux->next = temp;
+    return (aux->next);
 }
 
 /**
@@ -45,31 +45,31 @@ stack_t *add_dnodeint_end(stack_t **head, const int n)
  */
 stack_t *add_dnodeint(stack_t **head, const int n)
 {
-	stack_t *temp;
+    stack_t *temp;
 
-	if (head == NULL)
-		return (NULL);
-	temp = malloc(sizeof(stack_t));
-	if (!temp)
-	{
-		dprintf(2, "Error: malloc failed\n");
-		free_vglo();
-		exit(EXIT_FAILURE);
-	}
-	temp->n = n;
-	/*Careful with the first time*/
-	if (*head == NULL)
-	{
-		temp->next = *head;
-		temp->prev = NULL;
-		*head = temp;
-		return (*head);
-	}
-	(*head)->prev = temp;
-	temp->next = (*head);
-	temp->prev = NULL;
-	*head = temp;
-	return (*head);
+    if (head == NULL)
+        return (NULL);
+    temp = malloc(sizeof(stack_t));
+    if (!temp)
+    {
+        dprintf(2, "Error: malloc failed\n");
+        free_montyGlobals();
+        exit(EXIT_FAILURE);
+    }
+    temp->n = n;
+    /*Careful with the first time*/
+    if (*head == NULL)
+    {
+        temp->next = *head;
+        temp->prev = NULL;
+        *head = temp;
+        return (*head);
+    }
+    (*head)->prev = temp;
+    temp->next = (*head);
+    temp->prev = NULL;
+    *head = temp;
+    return (*head);
 }
 
 /**
@@ -80,11 +80,11 @@ stack_t *add_dnodeint(stack_t **head, const int n)
  */
 void free_dlistint(stack_t *head)
 {
-	stack_t *tmp;
+    stack_t *tmp;
 
-	while ((tmp = head) != NULL)
-	{
-		head = head->next;
-		free(tmp);
-	}
+    while ((tmp = head) != NULL)
+    {
+        head = head->next;
+        free(tmp);
+    }
 }
